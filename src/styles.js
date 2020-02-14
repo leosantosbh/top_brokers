87df74b811a3152c9df7c2599aec9d1b7841a07d
leosantosbh <soundsystem07@gmail.com>
2020-02-13 23:15:37 -0300

    Ajustes para Hack

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ContainerPopup = styled.div``;
+
+export const TitleApto = styled.text`
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const TextApto = styled.text`
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+`;
